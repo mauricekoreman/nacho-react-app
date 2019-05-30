ab33a619395c3d65bd1ef363c2fac2c4ba6736d0
Maurice Koreman <mg.koreman99@gmail.com>
2019-05-30 12:12:25 +0200

    api werkend

--- ns-react/src/App.js
+++ ns-react/src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import SightseeingComponent from './SightseeingComponent';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+
+  // To show a photo in our React app, we need to store the API response in state.
+  constructor(props) {
+    super(props);
+    this.state = {
+      imageURL: '',
+    }
+  }
+
+  componentDidMount() {
+    const BASE_URL = "https://www.triposo.com/api/20181213/";
+    const CATEGORIE_LOCATION = "location.json?";
+    const CATEGORIE_POI = "poi.json?";
+    const CATEGORIE_TAG = "tag.json?";
+    const CATEGORIE_TOUR = "tour.json?";
+    const CATEGORIE_ARTICLE = "article.json?";
+    const CATEGORIE_CITY_WALK = "city_walk.json?";
+    const CATEGORIE_LOCAL = "local_highlights.json?";
+    const CATEGORIE_DAY = "day_planner.json?";
+    const CATEGORIE_SCORE = "local_score.json?";
+
+    const API_TOKEN = "&token=v56oi7tj6zdoweync49h0h3vyddtd13x";
+    const ACCOUNT = "&account=P8TOBP16"
+    const LOCATION = "location_id="
+
+    const location = "Amsterdam";
+
+    // Sightseeing
+    axios.get(BASE_URL + CATEGORIE_POI + LOCATION + "Amsterdam" + "&order_by=-score&fields=name,coordinates,score,images,snippet" + ACCOUNT + API_TOKEN)
+    .then(res => {
+      console.log(res.data.results);
+      this.setState({ name: res.data.results[0].name});
+      this.setState({ text: res.data.results[0].snippet})
+      this.setState({ image: res.data.results[0].images[2].sizes.thumbnail.url });
+    })
+    .catch(error => {
+      console.log(error);
+    });
+
+    // Food
+    // axios.get(BASE_URL + C)
+  };
+
+  render() {
+    const { name } = this.state;
+    const { text } = this.state;
+    const { image } = this.state
+    return (
+      <div className="container">
+        <SightseeingComponent
+          name={ name }
+          text={ text }
+          image={ image }
+        />
+      </div>
+    );
+  }
+};
 
 export default App;
---
+++ ns-react/src/SightseeingComponent.js
@@ -0,0 +1,22 @@
+import React from "react";
+import './SightseeingComponent.css';
+
+class SightseeingComponent extends React.Component {
+  render() {
+    return (
+      <div className="img-card mdl-card mdl-shadow--2dp">
+        <div className="mdl-card__title mdl-card--expand" style={{ backgroundImage: `url(${this.props.image})`}}>
+          <h2 className="mdl-card__title-text">{ this.props.name }</h2>
+        </div>
+        <div className="mdl-card__supporting-text">{ this.props.text }</div>
+        <div className="mdl-card__actions mdl-card--border">
+          <a href="google.com" className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+            View updates
+          </a>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default SightseeingComponent;
