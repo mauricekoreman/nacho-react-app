fdc817c3f49aea16fd9c06e048ca07e17082223a
robertPaardekooper <paardekooper.robert@gmail.com>
2019-06-07 21:08:05 +0200

    Contactpagina gefixt
    Linken naar language page gaat nog niet automatisch pas na f5 ingedrukt te hebben

--- ns-react/src/components/Contact.js
+++ ns-react/src/components/Contact.js
@@ -2,41 +2,63 @@ import React from "react";
 import "../css/style.css";
 import vlaggenMenu from "../img/vlaggenMenu.png";
 import {Helmet} from 'react-helmet';
+import Language from './Language';
 
-export class Contact extends React.Component{
-  onArrowBackClicked = () => {
-    console.log("op terug geklikt");
+import {BrowserRouter as Router, Route, Link, Redirect, Switch} from "react-router-dom";
+
+export class Contact extends React.Component {
+  onMobileClicked = () => {
+    window.open('tel:+31307515155');
   };
-  onVlaggenMenuClicked = () => {
-    console.log("op vlaggenMenu geklikt");
+  onUrlClicked = () => {
+    window.location.href = 'http://www.ns.nl';
   };
- 
+  onMailClicked = () => {
+    window.location.href = "mailto:info@ns.nl";
+  };
+  onTwitterClicked = () => {
+    window.location.href = 'https://twitter.com/ns_online';
+  };
+
   render() {
-    return (<div>
-      <Helmet>
-        <style>{'body {background: linear-gradient(#fcc63f, white); background-repeat: no-repeat;}'}</style>
-      </Helmet>
-      <img className="vlaggenMenu" src={vlaggenMenu} onClick={this.onVlaggenMenuClicked}></img>
-      <h1 className="contact-heading-primary">Contact</h1>
-    <div className="contact-container">
-        <div className="contact-container-tel">
-          <i className="fa fa-mobile"></i>
-          <p className="contact-container-text">030 751 5155</p>
+    return (
+      <Router>
+      <div>
+        <Helmet>
+          <style>
+            {
+              'body {background: linear-gradient(#fcc63f, white); background-repeat: no-repeat;}'
+            }</style>
+        </Helmet>
+        <Link to="/language">
+          <img className="vlaggenMenu" src={vlaggenMenu}></img>
+        </Link>
+        <h1 className="contact-heading-primary">Contact</h1>
+        <div className="contact-container">
+          <div className="contact-container-tel">
+            <i className="fa fa-mobile" onClick={this.onMobileClicked}></i>
+            <p className="contact-container-text" onClick={this.onMobileClicked}>030 751 5155</p>
+          </div>
+          <div className="contact-container-url">
+            <i className="fa fa-globe" onClick={this.onUrlClicked}></i>
+            <p className="contact-container-text" onClick={this.onUrlClicked}>www.ns.nl</p>
+          </div>
+          <div className="contact-container-mail">
+            <i className="fa fa-at" onClick={this.onMailClicked}></i>
+            <p className="contact-container-text" onClick={this.onMailClicked}>info@ns.nl</p>
+          </div>
+          <div className="contact-container-social">
+            <i className="fa fa-twitter" onClick={this.onTwitterClicked}></i>
+            <p className="contact-container-text" onClick={this.onTwitterClicked}>@NS_online</p>
+          </div>
         </div>
-        <div className="contact-container-url">
-          <i className="fa fa-globe"></i>
-          <p className="contact-container-text">www.ns.nl</p>
-        </div>
-        <div className="contact-container-mail">
-          <i className="fa fa-at"></i>
-          <p className="contact-container-text">info@ns.nl</p>
-        </div>
-        <div className="contact-container-social">
-          <i className="fa fa-twitter"></i>
-          <p className="contact-container-text">@NS_online</p>
-        </div>
-      </div>
-    </div>);
+        <Switch>
+          <Route path="/language" exact="exact" component={Language}/>
+        </Switch>
+
+    </div>
+  </Router>
+  );
   };
 }
 
--- ns-react/src/components/LanguagePage.js
+++ ns-react/src/components/LanguagePage.js
@@ -1,10 +1,5 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Link,
-  Redirect
-} from "react-router-dom";
+import {BrowserRouter as Router, Route, Link, Redirect} from "react-router-dom";
 import Language from "./Language";
 import "../css/style.css";
 import Contact from "./Contact"
@@ -13,55 +8,41 @@ import vlagUK from "../img/UK.png";
 import vlagSP from "../img/SP.png";
 import vlagFK from "../img/FK.png";
 import {Helmet} from 'react-helmet';
-export class LanguagePage extends React.Component{
+export class LanguagePage extends React.Component {
 
   cardClicked = id => {
     console.log("je klikt op taal: " + id);
   };
 
   render() {
-    return (
-      <div>
-        <Helmet>
-          <style>{'body { background-color: white; background-repeat: no-repeat;}'}</style>
-        </Helmet>
-      <h3 className="language-title" >Pick your language</h3>
+    return (<div>
+      <Helmet>
+        <style>
+          {
+            'body { background-color: white; background-repeat: no-repeat;}'
+          }</style>
+      </Helmet>
+      <h3 className="language-title">Pick your language</h3>
       <div className="grid-container">
         <div className="grid-item">
-          <Language
-            id="Nederlands"
-            cardClicked={this.cardClicked}
-            background={vlagNL}
-          />
+          <Language id="Nederlands" cardClicked={this.cardClicked} background={vlagNL}/>
         </div>
         <div className="grid-item">
-          <Language
-            id="Engels"
-            cardClicked={this.cardClicked}
-            background={vlagUK}
-          />
+          <Language id="Engels" cardClicked={this.cardClicked} background={vlagUK}/>
         </div>
         <div className="grid-item">
-          <Language
-            id="Spaans"
-            cardClicked={this.cardClicked}
-            background={vlagSP}
-          />
+          <Language id="Spaans" cardClicked={this.cardClicked} background={vlagSP}/>
         </div>
         <div className="grid-item">
-          <Language
-            id="Frans"
-            cardClicked={this.cardClicked}
-            background={vlagFK}
-          />
+          <Language id="Frans" cardClicked={this.cardClicked} background={vlagFK}/>
         </div>
       </div>
       <div>
         <button className="contact-btn">
-        <Link to="/Contact" className="contact-btn--link">Contact</Link></button>
+          <Link to="/Contact" className="contact-btn--link">Contact</Link>
+        </button>
       </div>
-      </div>
-    );
+    </div>);
   }
 }
 export default LanguagePage;
