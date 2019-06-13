0c4a8ecefdf1b2c239d7691843bdfbf72e110a55
Warner Hoefakker <warner.hoefakker@live.nl>
2019-06-13 15:21:54 +0200

    CityOfferPage
    Nieuwe versie van de CityOfferPage

--- ns-react/src/components/CityOfferPage.js
+++ ns-react/src/components/CityOfferPage.js
@@ -1,70 +1,98 @@
 import React from "react";
-import axios from "axios";
 import "../css/style.css";
+import "../sass/CityOfferPage.scss";
+import axios from "axios";
 import SwipeableViews from 'react-swipeable-views';
+import {BrowserRouter as Router, Route, Link, Redirect, Switch} from "react-router-dom";
 import {Helmet} from 'react-helmet';
 import Language from './Language';
-import {
-  BrowserRouter as Router,
-  Route,
-  Link,
-  Switch,
-  Redirect
-} from "react-router-dom";
+import {naam_stad} from './GpsComponent';
+import GpsComponent from './GpsComponent';
+const styles = {
+  slide: {
+    padding: 15,
+    minHeight: 100,
+    marginLeft: 30,
+    marginRight: 30,
+    marginTop: 150,
+    borderRadius: 25,
+    color: '#212B5C',
+  },
+  slide1: {
+    backgroundColor: '#fff',
+    // paddingLeft: 100,
+  },
+  slide2: {
+    backgroundColor: '#fff',
+    // paddingLeft: 100,
+  },
+  text: {
+    paddingTop: 10,
+    paddingBottom:-10,
+    fontSize: 30,
+  }
+  // slide3: {
+  //   backgroundColor: '#fff',
+  // },
+};
 
 class CityOfferPage extends React.Component{
-
-  constructor(props){
-    super(props);
+  // To show a photo in our React app, we need to store the API response in state.
+  constructor() {
+    super()
     this.state = {
-      imageURL: '',
+      city:'',
+      image:''
     }
+
   }
 
   componentDidMount() {
-    const BASE_URL = "https://www.triposo.com/api/20181213/";
-    const CATEGORIE_LOCATION = "location.json?";
-    const CATEGORIE_POI = "poi.json?";
+    const BASE_URL = "http://95.179.178.130/api/";
+    const CITY = {naam_stad};
 
-    const TAG_LABELS = "&tag_labels=";
-    const LOCATION = "location_id=";
-
-    const ACCOUNT = "&account=P8TOBP16";
-    const API_TOKEN = "&token=v56oi7tj6zdoweync49h0h3vyddtd13x";
-
+    console.log({naam_stad});
+    // Sightseeing
     axios.all([
-      axios.get(BASE_URL + CATEGORIE_POI + LOCATION + "Amsterdam" + "&order_by=-score&fields=name,score,images,snippet&count=1" + ACCOUNT + API_TOKEN )
+      axios.get(BASE_URL+CITY),
+      console.log(BASE_URL+CITY)
     ])
-    .then(axios.spread((cityImg)=> {
+    .then(axios.spread((activityRes) => {
       this.setState({
-        name: cityImg.data.results[0].name,
-        image: cityImg.data.results[0].images[2].sizes.thumbnail.url
+        name: activityRes.data.stad
+        // image: activityRes.merk_naam
       });
     }));
-  }
-
-  render(){
-    return (
-      <div className="sightseeing-container">
-        <Helmet>
-          <style>{'body {background: linear-gradient(#fcc63f, white); background-repeat: no-repeat;}'}</style>
-        </Helmet>
-        <div>
-          <button className="vlaggenMenu">
-            <Link to="/Language" className="vlaggenMenu-link"></Link>
-          </button>
-        </div>
-          <div className="mdl-card__title mdl-card--expand" style={{ backgroundImage: `url(${this.props.image})`}}>
-            <h2 className="mdl-card__title-text">{ this.props.name}</h2>
-          </div>
-          <div className="mdl-card__supporting-text">{ this.props.name}</div>
-          <div className="mdl-card__actions mdl-card--border">
-            <a href={"https://en.wikipedia.org/wiki/" + this.props.actname} className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
-              More info
-            </a>
-          </div>
-      </div>
-    );
   };
+
+
+render() {
+// const CityOfferPage = () => {
+  return (
+    <div>
+      <Helmet>
+        <style>{'body { background-color: #FCC63F; background-repeat: no-repeat; font-family: Montserrat}'}</style>
+      </Helmet>
+      <div>
+        <button className="vlaggenMenu">
+          <Link to="/Language" className="vlaggenMenu-link"></Link>
+        </button>
+      </div>
+
+      <SwipeableViews enableMouseEvents>
+        <div style={Object.assign({}, styles.slide, styles.slide1)}>
+          <img src="../img/Leiden.jpg" alt="" />
+          <h1 style={Object.assign({}, styles.text)}>{this.state.name}</h1>
+          </div>
+
+        <div style={Object.assign({}, styles.slide, styles.slide2)}>
+          <img src="../img/Amsterdam.jpg" alt="" />
+          <h1 style={Object.assign({}, styles.text)}>{this.state.name}</h1>
+        </div>
+      </SwipeableViews>
+    </div>
+
+  );
+}
 }
 export default CityOfferPage;
