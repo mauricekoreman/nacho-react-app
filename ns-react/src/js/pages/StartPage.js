9c017313e3139efc42f6e433fa1ca43defa347c4
robertPaardekooper <paardekooper.robert@gmail.com>
2019-06-21 16:12:04 +0200

    heel veel .scss veranderingen
    startpage
    cityofferpage
    gpscomponent

--- ns-react/src/js/pages/StartPage.js
+++ ns-react/src/js/pages/StartPage.js
@@ -2,39 +2,31 @@ import React from "react";
 import "../../css/style.css";
 import vlaggenMenu from "../../img/vlaggenMenu.png";
 import "../../sass/StartPage.scss";
-import {
-  Helmet
-} from 'react-helmet';
+import {Helmet} from 'react-helmet';
 import Language from '../components/Language';
-import {
-  BrowserRouter as Router,
-  Route,
-  Link,
-  Switch,
-  Redirect
-} from "react-router-dom";
+import {BrowserRouter as Router, Route, Link, Switch, Redirect} from "react-router-dom";
 
 export class StartPage extends React.Component {
 
-    render() {
-        return (
-          <div>
-            <Helmet>
-              <style>{'body {background: linear-gradient(#fcc63f, white); background-repeat: no-repeat;}'}</style>
-            </Helmet>
-            <div className = "gps-container" />
-            <div className = "gps-header" />
-            <button className = "vlaggenMenu" >
-              <Link to = "../Language" className = "vlaggenMenu-link"></Link>
-            </button>
-            <img className= "conducteurPic" src="../img/conducteur.jpg" alt="" />
-            <div className="tekstBlok">
-              <p className="introductie">First of all,
-Welcome to the Netherlands!!</p>
-              <Link to="/GpsComponent" className="start_knop">Get started</Link>
-            </div>
-          </div>
-        );
-      };
-    }
-      export default StartPage;
+  render() {
+    return (<div>
+      <Helmet>
+        <style>
+          {
+            'body {background: linear-gradient(#fcc63f, white); background-repeat: no-repeat;}'
+          }</style>
+      </Helmet>
+      <div className="gps-container"/>
+      <div className="gps-header"/>
+      <button className="vlaggenMenu">
+        <Link to="../Language" className="vlaggenMenu-link"></Link>
+      </button>
+      <img className="conducteurPic" src="../img/conducteur2.jpg" alt=""/>
+      <div className="tekstBlok">
+        <p className="introductie">First of all, Welcome to the Netherlands!!</p>
+        <Link to="/GpsComponent" className="start_knop">Get started</Link>
+      </div>
+    </div>);
+  };
+}
+export default StartPage;
